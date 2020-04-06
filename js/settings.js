8b77ab1f7b9e6a3164cc30ff999cf773a63fc3cb
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-04-06 14:51:40 +0200

    change with and border of game choices

--- js/settings.js
+++ js/settings.js
@@ -13,9 +13,9 @@ class GameMode {
 class Settings {
     
     modes = [
-        new GameMode("img/settings/size-small.png"),
-        new GameMode("img/settings/size-medium.png"),
-        new GameMode("img/settings/size-big.png"),
+        new GameMode("img/settings/size-small.png", 6),
+        new GameMode("img/settings/size-medium.png", 12),
+        new GameMode("img/settings/size-big.png", 24),
     ];
 
     dealerNames = [
